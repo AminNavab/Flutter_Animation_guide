35482b2ce2796d355e9cb619d14d0ab2d5c42f24
AminNavabzade <aminnavabzade@gmail.com>
2026-08-19 18:45:10 +0330

    add main Screan file

---
+++ lib/Model/models.dart
@@ -0,0 +1,19 @@
+List animationstype = ["Implicit Animations", "Explicit Animations"];
+List implicitAnimationName = [
+  "AnimatedOpacity",
+  "AnimatedContainer",
+  "AnimatedPositioned",
+  "AnimatedRotation",
+  "AnimatedScale",
+  "AnimatedAlign",
+  "AnimatedPadding",
+  "AnimatedSize",
+  "AnimatedCrossFade",
+  "AnimatedDefaultTextStyle",
+  "AnimatedPhysicalModel",
+  "AnimatedSwitcher",
+  "AnimatedFractionallySizedBox",
+  "AnimatedTheme",
+  "SliverAnimatedOpacity",
+  "TweenAnimationBuilder",
+];
---
+++ lib/View/mainScrean.dart
@@ -0,0 +1,57 @@
+import 'package:animations/Model/models.dart';
+import 'package:flutter/material.dart';
+
+class Mainscrean extends StatelessWidget {
+  const Mainscrean({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var bodyMargin = MediaQuery.of(context).size.width / 8;
+    var texttheme = Theme.of(context).textTheme;
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+              left: bodyMargin,
+              right: bodyMargin,
+              top: 20,
+              bottom: 20,
+            ),
+            child: Container(
+              height: 100,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.lightGreen,
+                borderRadius: BorderRadius.circular(32),
+              ),
+              child: Center(
+                child: Text(animationstype[0], style: texttheme.bodyLarge),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: bodyMargin,
+              right: bodyMargin,
+              top: 20,
+              bottom: 20,
+            ),
+            child: Container(
+              height: 100,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.lightGreen,
+                borderRadius: BorderRadius.circular(32),
+              ),
+              child: Center(
+                child: Text(animationstype[1], style: texttheme.bodyLarge),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
